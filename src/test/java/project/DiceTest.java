02cd542d4036a2cbd45b9a6979ca1a62c336be77
colin-marsh <colinmarsh2000@gmail.com>
2022-10-13 17:36:54 -0400

    Test Commit - Roll die, check active

---
+++ src/test/java/project/DiceTest.java
@@ -0,0 +1,52 @@
+package project;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class DiceTest
+    extends TestCase
+{
+
+    /**
+     * @return the suite of tests being tested
+
+    public static Test suite()
+    {
+        return new TestSuite( DiceTest.class );
+    }
+
+     * Rigourous Test :-)
+     */
+    public void testDie(){
+        Die d = new Die();
+        assertEquals(Roll.UNROLLED, d.getRoll());
+    }
+
+    public void testRollDie()
+    {
+        Die d = new Die();
+        d.rollDie();
+        if (d.getRoll() == Roll.SKULL){
+            assertEquals(false, d.getActive());
+        }else{
+            assertEquals(true, d.getActive());
+        }
+    }
+
+    public void testKeepDie()
+    {
+        Die d = new Die();
+        d.rollDie();
+        if (d.getRoll() == Roll.SKULL){
+            assertEquals(false, d.getActive());
+        }else{
+            d.keep();
+            assertEquals(false, d.getActive());
+        }
+    }
+
+}
---
+++ src/test/java/project/TestSuite.java
@@ -0,0 +1,17 @@
+package project;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * TestSuite is a test suite fore all tests. All testcase files should be inside the Suite
+ *
+ * @author Sebastian Gadzinski
+ * @author Tarnish
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {DiceTest.class} )
+public class TestSuite {
+
+}
