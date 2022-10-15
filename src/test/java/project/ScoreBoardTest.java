6f2770d31c802e88f9a72badacf43b7bb9cdee68
colin-marsh <colinmarsh2000@gmail.com>
2022-10-14 21:34:14 -0400

    Test Case - Row41 test

---
+++ src/test/java/project/ScoreBoardTest.java
@@ -0,0 +1,22 @@
+package project;
+import junit.framework.TestCase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScoreBoardTest extends TestCase{
+
+    public void testCalculateDiceScoreRow41(){
+        Map<Roll,Integer> diceRolls = new HashMap<Roll, Integer>();
+        Scoreboard sb = new Scoreboard(1);
+        diceRolls.put(Roll.COIN,0);
+        diceRolls.put(Roll.DIAMOND,0);
+        diceRolls.put(Roll.SWORD,5);
+        diceRolls.put(Roll.SKULL,3);
+        diceRolls.put(Roll.PARROT,0);
+        diceRolls.put(Roll.MONKEY,0);
+        int score = sb.calculateDiceScore(diceRolls);
+        assertEquals(0 ,score);
+    }
+
+}
